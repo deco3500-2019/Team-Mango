4d8c9c7860c10e4af1858549e7b443035cf87c65
Wally886 <44498554+WallyYoung886@users.noreply.github.com>
2019-10-17 14:08:42 +1000

    Nearly Final Version

--- js/eventData.js
+++ js/eventData.js
@@ -1,33 +1,36 @@
 const eventData = [{
         location: "Queen St",
-        timeStart: "2019-10-02",
-        timeEnd: "2019-10-03",
+        timeStart: "2019-10-01T13:00",
+        timeEnd: "2019-10-01T15:00",
         title: "Spainish Culture",
         description: "Very good event, come and join",
+        requiredJob:["Photographer", "Designer", "Back-up"],
         cover: "images/profile.jpeg",
-        tag: [],
+        tag: ["eco","tec"],
         owner: "Spain Co.",
         id: "1570760202249"
     },
     {
-        location: "Queen St",
-        timeStart: "2019-10-02",
-        timeEnd: "2019-10-02",
-        title: "Spainish Culture",
+        location: "Toowong",
+        timeStart: "2019-10-12T08:00",
+        timeEnd: "2019-10-12T11:00",
+        title: "Chines Culture",
         description: null,
+        requiredJob:["Back-up"],
         cover: "images/profile.jpeg",
-        tag: [],
+        tag: ["pol","eco"],
         owner: "Spain Co.",
         id: "1570760202300"
     },
     {
-        location: "Queen St",
-        timeStart: "2019-10-02",
-        timeEnd: "2019-10-04",
-        title: "Spainish Culture",
+        location: "Milton",
+        timeStart: "2019-10-20T13:00",
+        timeEnd: "2019-10-20T13:00",
+        title: "Aussie Culture",
         description: null,
+        requiredJob:["Photographer", "Back-up"],
         cover: "images/profile.jpeg",
-        tag: [],
+        tag: ["cul","tec"],
         owner: "Spain Co.",
         id: "1570760203456"
     },
@@ -37,12 +40,12 @@ const eventData = [{
 const volunteerData = [
     {
         volunteerId:"1",
-        signedEvent:"1570760203456",
         name:"Hugo Christie",
         gender:"Male",
         location:"Brisbane, Queensland",
-        profile:"images/profile.jpeg",
-        job:"Photographer",
+        email:"v123@vol.com",
+        tel: "1234555",
+        profile:"images/vol-profile.JPG",
         introduction: "in this area, user needs to introduce himself or herself, maybe 50-150 words? oh, I forget the number they mentioned before."+
         " In this  sketch, maybe 8-10 words a line. So, now  there has been 35 words. Let me type more to meet 50 words. ok, I will repeat these sentences."+
         "in this area, user needs to introduce himself or herself, maybe 50-150 words? oh, I forget the number they mentioned before. In this  sketch,"+
@@ -54,12 +57,12 @@ const volunteerData = [
     },
     {
         volunteerId:"2",
-        signedEvent:"1570760203456",
         name:"Wade Giles",
-        gender:"Male",
+        gender:"Female",
+        email:"v123@vol.com",
+        tel: "1234555",
         location:"Brisbane, Queensland",
-        profile:"images/profile.jpeg",
-        job:"Photographer",
+        profile:"images/vol-profile.JPG",
         introduction: "in this area, user needs to introduce himself or herself, maybe 50-150 words? oh, I forget the number they mentioned before."+
         " In this  sketch, maybe 8-10 words a line. So, now  there has been 35 words. Let me type more to meet 50 words. ok, I will repeat these sentences."+
         "in this area, user needs to introduce himself or herself, maybe 50-150 words? oh, I forget the number they mentioned before. In this  sketch,"+
@@ -71,6 +74,21 @@ const volunteerData = [
     }
 ];
 
+const matchReco = [
+    {
+        volunteerId:"1",
+        signedEvent:"1570760203456",
+        signedJob:"Photographer",
+        type:1,
+    },
+    {
+        volunteerId:"2",
+        signedEvent:"1570760203456",
+        signedJob:"Photographer",
+        type: 1,
+    }
+];
+
 
 const checkLocalStorage = () => {
     if (!localStorage.getItem("eventData")) {
@@ -79,15 +97,19 @@ const checkLocalStorage = () => {
     if(!localStorage.getItem("volunteerData")) {
         localStorage.setItem("volunteerData", JSON.stringify(volunteerData));
     }
+    if(!localStorage.getItem("matchReco")) {
+        localStorage.setItem("matchReco", JSON.stringify(matchReco));
+    }
 };
 
-const createNewEvent = (title, location, timeStart, timeEnd, cover, tag, description, owner, localEventData) => {
+const createNewEvent = (title, location, timeStart, timeEnd, requiredJob, cover, tag, description, owner, localEventData) => {
     localEventData.unshift({
         location: location,
         timeStart: timeStart,
         timeEnd: timeEnd,
         title: title,
         description: description,
+        requiredJob: requiredJob,
         cover: cover,
         tag: tag,
         owner: owner,
@@ -96,13 +118,14 @@ const createNewEvent = (title, location, timeStart, timeEnd, cover, tag, descrip
     localStorage.setItem("eventData", JSON.stringify(localEventData));
 };
 
-const updateEvent = (eventId, title, location, timeStart, timeEnd, cover, tag, description, owner, localEventData) => {
+const updateEvent = (eventId, title, location, timeStart, timeEnd, requiredJob, cover, tag, description, owner, localEventData) => {
     const newInfo = {
         location: location,
         timeStart: timeStart,
         timeEnd: timeEnd,
         title: title,
         description: description,
+        requiredJob: requiredJob,
         cover: cover,
         tag: tag,
         owner: owner,
@@ -115,6 +138,15 @@ const updateEvent = (eventId, title, location, timeStart, timeEnd, cover, tag, d
         }
     }
 };
+
+const updateVolunteer = (volunteer, localVolunteerData) => {
+    for (let i = 0; i < localVolunteerData.length; i++) {
+        if (localVolunteerData[i].volunteerId == volunteer.volunteerId) {
+            localVolunteerData.splice(i, 1, volunteer);
+            localStorage.setItem("volunteerData", JSON.stringify(localVolunteerData));
+        }
+    }
+}
 
 function getEventDetail(eventId) {
     const localEventData = JSON.parse(localStorage.getItem("eventData"));
